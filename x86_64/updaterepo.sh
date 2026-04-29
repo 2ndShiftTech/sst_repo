7d9a3cf178e0cd020e9f47b8ec0bb075a8619bf2
2ndShiftTech <secondshifttech@proton.me>
2026-04-29 05:53:20 +0000

    updated

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-rm buildra_repo*
+rm sst-arch_repo*
 
 echo "repo-add"
-repo-add -n -R buildra_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R sst-arch_repo.db.tar.gz *.pkg.tar.zst
 
 sleep 1
 
-rm buildra_repo.db
+rm sst-arch_repo.db
 
-rm buildra_repo.files
+rm sst-arch_repo.files
 
-mv buildra_repo.db.tar.gz buildra_repo.db
+mv sst-arch_repo.db.tar.gz sst-arch_repo.db
 
-mv buildra_repo.files.tar.gz buildra_repo.files
+mv sst-arch_repo.files.tar.gz sst-arch_repo.files
 
 echo "####################################"
 echo "Repo Updated!!"
