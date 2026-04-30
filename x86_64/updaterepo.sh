29119ca167dd9b9d8423e88c7a9983d14758e12d
2ndShiftTech <secondshifttech@proton.me>
2026-04-30 19:58:30 +0000

    updated Repo

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -12,9 +12,9 @@ rm sst-repo.db
 
 rm sst-repo.files
 
-mv sst-arch_repo.db.tar.gz sst-repo.db
+mv sst-repo.db.tar.gz sst-repo.db
 
-mv sst-arch_repo.files.tar.gz sst-repo.files
+mv sst-repo.files.tar.gz sst-repo.files
 
 echo "####################################"
 echo "Repo Package List Updated!!"
