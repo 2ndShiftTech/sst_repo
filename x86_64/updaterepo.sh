a6c55481ec2dc67167becec0e44f5b5006b7a11e
2ndShiftTech <secondshifttech@proton.me>
2026-04-30 18:14:23 +0000

    changes for the better

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-rm sst-arch_repo*
+rm sst-repo*
 
 echo "repo-add"
-repo-add -n -R sst-arch_repo.db.tar.gz *.pkg.tar.zst
+repo-add -n -R sst-repo.db.tar.gz *.pkg.tar.zst
 
 sleep 1
 
-rm sst-arch_repo.db
+rm sst-repo.db
 
-rm sst-arch_repo.files
+rm sst-repo.files
 
-mv sst-arch_repo.db.tar.gz sst-arch_repo.db
+mv sst-arch_repo.db.tar.gz sst-repo.db
 
-mv sst-arch_repo.files.tar.gz sst-arch_repo.files
+mv sst-arch_repo.files.tar.gz sst-repo.files
 
 echo "####################################"
 echo "Repo Updated!!"
